9125b35d072e999e86da1d9a07a82f3fbb527a80
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-18 16:36:16 -0800

    Add new API call for quotes
    Co-authored by: Kim Brannian <brannian@gmail.com>
    dquote> Co-authored-by: Chase Stokes <chasehstokes@gmail.com>

--- src/theOneAPI.js
+++ src/theOneAPI.js
@@ -19,8 +19,28 @@ export default class TheOne {
       .catch(function(error) {
         return error;
       });
-  } 
-}
+  }
+  static quotes(quote) {
+    const newHeaders = new Headers();
+    newHeaders.append("Authorization", "Bearer jPfPgbaSQGmObc8HixcC");
+    
+    const requestOptions2 = {
+      method: 'GET',
+      headers: newHeaders,
+      redirect: 'follow'
+    };
 
+    return fetch(`https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfea0/quote`, requestOptions)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
 
 
